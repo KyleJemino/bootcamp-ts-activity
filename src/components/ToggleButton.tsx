435c6b44ddd54f636ec79102edf86fbb66b046e9
Joseph Villanueva <josephgvillanueva24@gmail.com>
2023-03-24 11:31:02 +0800

    last minute change

--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -1,4 +1,8 @@
-type ToggleButtonType = { buttonText: string; state: boolean; setState: any };
+type ToggleButtonType = {
+  buttonText: string;
+  state: boolean;
+  setState: (state: boolean) => void;
+};
 
 const ToggleButton = ({ buttonText, state, setState }: ToggleButtonType) => {
   return <button onClick={() => setState(!state)}>{buttonText}</button>;
